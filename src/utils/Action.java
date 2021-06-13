8d870b651447d75c7251e56329fa1eb38295c3e4
Wu-Jun Pei <jimpei8989@gmail.com>
2021-06-13 17:06:45 +0800

    Add Coordinate to Action

--- src/utils/Action.java
+++ src/utils/Action.java
@@ -1,15 +1,23 @@
 package utils;
 
 public enum Action {
-    DOWN("DOWN"), RIGHT("RIGHT"), UP("UP"), LEFT("LEFT"), NO_OP("NO_OP"), ATTACK("ATTACK");
+    DOWN("DOWN", new Coordinate<Integer>(1, 0)), RIGHT("RIGHT", new Coordinate<Integer>(0, 1)),
+    UP("UP", new Coordinate<Integer>(-1, 0)), LEFT("LEFT", new Coordinate<Integer>(0, -1)),
+    NO_OP("NO_OP", new Coordinate<Integer>(0, 0)), ATTACK("ATTACK", new Coordinate<Integer>(0, 0));
 
     private final String name;
+    private final Coordinate<Integer> coord;
 
-    Action(String name) {
+    Action(String name, Coordinate<Integer> coord) {
         this.name = name;
+        this.coord = coord;
     }
 
     public String toString() {
         return this.name;
+    }
+
+    public Coordinate<Integer> getCoord() {
+        return this.coord;
     }
 }
--- src/utils/Direction.java
+++ src/utils/Direction.java
@@ -1,8 +1,8 @@
 package utils;
 
 public enum Direction {
-    DOWN("DOWN", new Coordinate<Integer>(1, 0)), RIGHT("RIGHT", new Coordinate<Integer>(0, 1)), UP(
-            "UP", new Coordinate<Integer>(-1, 0)), LEFT("LEFT", new Coordinate<Integer>(0, -1));
+    DOWN("DOWN", new Coordinate<Integer>(1, 0)), RIGHT("RIGHT", new Coordinate<Integer>(0, 1)),
+    UP("UP", new Coordinate<Integer>(-1, 0)), LEFT("LEFT", new Coordinate<Integer>(0, -1));
 
     private final String name;
     private Coordinate<Integer> coord;
