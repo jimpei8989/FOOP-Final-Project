bc576020a1d6af2f405c979c56bb11e31b1b94b3
Wu-Jun Pei <jimpei8989@gmail.com>
2021-06-13 17:01:13 +0800

    Add coordinate to direction

--- src/utils/Direction.java
+++ src/utils/Direction.java
@@ -1,15 +1,22 @@
 package utils;
 
 public enum Direction {
-    DOWN("DOWN"), RIGHT("RIGHT"), UP("UP"), LEFT("LEFT");
+    DOWN("DOWN", new Coordinate<Integer>(1, 0)), RIGHT("RIGHT", new Coordinate<Integer>(0, 1)), UP(
+            "UP", new Coordinate<Integer>(-1, 0)), LEFT("LEFT", new Coordinate<Integer>(0, -1));
 
     private final String name;
+    private Coordinate<Integer> coord;
 
-    Direction(String name) {
+    Direction(String name, Coordinate<Integer> coord) {
         this.name = name;
+        this.coord = coord;
     }
 
     public String toString() {
         return this.name;
+    }
+
+    public Coordinate<Integer> getCoord() {
+        return this.coord;
     }
 }
