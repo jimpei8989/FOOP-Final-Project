3c7af6023078f0a2d4f84e9a806d78e3aadb6a9e
andy94077 <andy94077@gmail.com>
2021-06-13 17:46:36 +0800

    Add state

---
+++ src/model/state/State.java
@@ -0,0 +1,50 @@
+package model.state;
+
+import model.Tickable;
+import utils.Active;
+
+public abstract class State implements Tickable, Active {
+    public String name;
+    private Pacman target;
+    private int turn = 1;
+
+    public State(String name, Pacman target) {
+        this.name = name;
+        this.target = target;
+    }
+
+    public State(String name, Pacman target, int turn) {
+        this.name = name;
+        this.target = target;
+        this.turn = turn;
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
+
+    public void onTurnBegin() {
+    }
+
+    @Override
+    public void onTurnEnd() {
+        this.turn--;
+    }
+
+    @Override
+    public void onRoundBegin() {
+    }
+
+    public void onRoundEnd() {
+    }
+
+    public void onStateWillChange() {
+    }
+
+    @Override
+    public boolean isActive() {
+        return this.turn > 0;
+    }
+
+}
