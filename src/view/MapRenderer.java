ec913b6f98349171e1ab47bc1eddd081bc6fa316
Dylan Hsieh <d4002x400288@gmail.com>
2021-06-20 15:36:17 +0800

    Add renderers

---
+++ src/view/MapRenderer.java
@@ -0,0 +1,17 @@
+package view;
+
+import java.awt.*;
+
+import model.maps.Map;
+
+public class MapRenderer implements Renderable {
+    private Map map;
+
+    public MapRenderer(Map map) {
+        this.map = map;
+    }
+
+    public void render(Graphics g) {
+
+    }
+}
---
+++ src/view/PacmanRenderer.java
@@ -0,0 +1,16 @@
+package view;
+
+import java.awt.*;
+
+import model.Pacman;
+
+public class PacmanRenderer implements Renderable {
+    private Pacman pacman;
+
+    public PacmanRenderer(Pacman pacman) {
+        this.pacman = pacman;
+    }
+
+    public void render(Graphics g) {
+    }
+}
