839ddbd47b8447bc749f12f4822b5cac0b76fba3
Dylan Hsieh <d4002x400288@gmail.com>
2021-06-27 00:27:16 +0800

    Refine getName() of Prop

--- src/model/prop/BigPointProp.java
+++ src/model/prop/BigPointProp.java
@@ -5,15 +5,11 @@ import utils.Coordinate;
 public class BigPointProp extends PointProp {
     public BigPointProp(Coordinate coord) {
         super(coord);
+        this.name = "BigPoint";
     }
 
     @Override
     protected int getPoint() {
         return 5;
-    }
-
-    @Override
-    public String getName() {
-        return "BigPoint";
     }
 }
--- src/model/prop/SmallPointProp.java
+++ src/model/prop/SmallPointProp.java
@@ -5,15 +5,11 @@ import utils.Coordinate;
 public class SmallPointProp extends PointProp {
     public SmallPointProp(Coordinate coord) {
         super(coord);
+        this.name = "SmallPoint";
     }
 
     @Override
     protected int getPoint() {
         return 1;
-    }
-
-    @Override
-    public String getName() {
-        return "SmallPoint";
     }
 }
